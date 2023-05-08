6baea44ca02506f70ab419fc69077c8a75de6c8e
divyachaudhari10 <126410788+divyachaudhari10@users.noreply.github.com>
2023-05-08 21:03:23 +0530

    Update OOP1.js

--- OOP1.js
+++ OOP1.js
@@ -21,3 +21,20 @@ var Obj352 = new Student("Komal", 98, 21, "Ahamednagar");
 var Obj482 = new Student("Kiti", 90, 28, "Pune");
 Obj352.Display();
 Obj482.Display();
+
+
+
+
+  }
+    Student.prototype.Display = function () {
+        console.log("Name of student :" + this.Name);
+        console.log("Name of student :" + this.Marks);
+        console.log("Name of student :" + this.Age);
+        console.log("Name of student :" + this.City);
+    };
+    return Student;
+}());
+var Obj352 = new Student("Komal", 98, 21, "Ahamednagar");
+var Obj482 = new Student("Kiti", 90, 28, "Pune");
+Obj352.Display();
+Obj482.Display();
